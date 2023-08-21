aec3351dd05932cdf676a442bef767804e014bcb
Avila Esteban <cqasss@gmail.com>
2023-08-21 16:15:47 -0300

    nuevos campos formumario

--- webapp/models.py
+++ webapp/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=20, verbose_name='username')
+    nombre = models.CharField(max_length=20, verbose_name='Nombre')
+    telefono = models.IntegerField(max_length=20, verbose_name='Telefono')
+    mail = models.CharField(max_length=20, verbose_name='E-mail')
+    mensaje = models.CharField(max_length=20, verbose_name='Mensaje')
 
     def __str__(self):
         return self.username
