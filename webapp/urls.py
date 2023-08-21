8224d575e8f01b875ead9c19c77934a8fbbac665
dhubalde <dalilahubalde@hotmail.com>
2023-08-21 15:47:44 -0300

    Formulario

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,7 +4,5 @@ from webapp import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('about/', views.about, name="about"),
-    path('contact/', views.contact, name="contact"),
     
-]
+]    
--- webapp/views.py
+++ webapp/views.py
@@ -5,15 +5,9 @@ from .forms import ContactForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-def contact(request):
-    
     form = ContactForm(request.POST or None, request.FILES or None)
-    return render(request, 'contact.html', {'form': form})
+    return render(request, 'index.html', {'form': form})
+
 
 def sevice(request):
     return render(request, 'login.html')
