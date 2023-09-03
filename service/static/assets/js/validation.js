bddd99ea09a146591091108b3d7b1ee0a213f3a1
dhubalde <dalilahubalde@hotmail.com>
2023-09-03 17:45:45 -0300

    Validaciones

--- service/static/assets/js/validation.js
+++ service/static/assets/js/validation.js
@@ -1,19 +1,29 @@
-const $formulario=document.getElementById('formulario');
-const $c_name=document.getElementById('nom_cliente');
+const formulario=document.getElementById('formulario');
+const c_name=document.getElementById('nom_cliente').value;
+const send=document.getElementById('send');
+const c_direccion=document.getElementById('dir_cliente').value;
+const c_dni=document.getElementById('dni_cliente').value;
 
 pattern_Nombre = '^[A-Z]*[a-z]{2,}[a-zA-Z ]*$'
 pattern_Direccion = '^[A-Z][a-zA-Z0-9 ]*$'
 pattern_soloNumeros = '^[0-9][0-9]*$'
 pattern_soloLetras = '^[A-Z][a-zA-Z ]*$'
 
-(function () {
-    console.log("estamos yendo")
-    $formulario.addEventListener('submit', function(e) {
-        let nombre=String($c_name.value).trim();
-        if(nombre.lenght === 0) {
-            alert("El NOMBRE DEL CLIENTE no puede ir vacio");
-            e.preventDefault();
-        }
-        
-    });
-});
+send.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (c_name.match(pattern_Nombre) && c_name.lenght == 0 && c_direccion.match(pattern_Direccion) && c_direccion.lenght == 0){
+        Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Debe comenzar con Mayúscula'
+                });
+    }else{
+        Swal.fire({
+            icon: 'success',
+            title: 'OK',
+            text: 'Bien!!!!'
+            });
+    }
+})
+
+
