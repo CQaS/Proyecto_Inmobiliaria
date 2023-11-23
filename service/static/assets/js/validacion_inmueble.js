350de7f11895efe9a09fc5279f8fbecf7d114f8e
Avila Esteban <40841825+CQaS@users.noreply.github.com>
2023-11-23 20:14:27 -0300

    buscar por fecha

--- service/static/assets/js/validacion_inmueble.js
+++ service/static/assets/js/validacion_inmueble.js
@@ -300,7 +300,7 @@ let dataTableIsInitialized = false
 const dataTableOptions = {
     "language": {
         "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-        
+
 
     },
     scrollX: "2000px",
@@ -348,14 +348,13 @@ const listInmuebles = async () => {
         data.inmueble.forEach((p, i) => {
             content += `
                 <tr>
-                    <td>${p.cod_referencia}</td>
-                    <td>${p.dir_inmueble}</td>
-                    <td>${p.tipo_inmueble}</td>
-                    <td>${p.valor_inmueble}</td>
-                    <td>${p.habitac_maxima}</td>
-                    <td>${p.tipo_servicio}</td>
-                    
-                    <td>
+                    <td  class="centered">${p.cod_referencia}</td>
+                    <td  class="centered">${p.dir_inmueble}</td>
+                    <td  class="centered">${p.tipo_inmueble}</td>
+                    <td  class="centered">${p.valor_inmueble}</td>
+                    <td  class="centered">${p.habitac_maxima}</td>
+                    <td  class="centered">${p.tipo_servicio}</td>                    
+                    <td  class="centered">
                         <button class='btn btn-sm'><i class='fa-solid fa-pencil'></i></button>
                         <button class='btn btn-sm'><i class='fa-solid fa-trash-can'></i></button>
                     </td>
@@ -371,4 +370,53 @@ if (typeof inmueble_Form == "undefined") {
     window.addEventListener("load", async () => {
         await initDataTable()
     })
+}
+
+porFecha.addEventListener('click', async () => {
+    f_i = f_ini.value
+    f_f = f_fin.value
+
+    if (f_i == '' || f_f == '') {
+        _alerta('Seleccion fechas')
+        return
+    }
+
+    let url = `/propiedad/buscar_por_fechas/${f_i}/${f_f}`
+
+    if (dataTableIsInitialized) {
+        dataTable.destroy()
+    }
+
+    await listInmueblesDisponibles(url)
+
+    dataTable = $("#datatable-reportes").DataTable(dataTableOptions)
+
+    dataTableIsInitialized = true
+})
+
+const listInmueblesDisponibles = async (url) => {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+
+        let content = ``
+        data.forEach((p, i) => {
+            content += `
+                <tr>
+                    <td  class="centered">${p.inmueble.cod_referencia}</td>
+                    <td  class="centered">${p.inmueble.dir_inmueble}</td>
+                    <td  class="centered">${p.inmueble.tipo_inmueble}</td>
+                    <td  class="centered">${p.inmueble.valor_inmueble}</td>
+                    <td  class="centered">${p.inmueble.habitac_maxima}</td>
+                    <td  class="centered">${p.inmueble.tipo_servicio}</td>                    
+                    <td  class="centered">
+                        <a href="/propiedad/detalles/${p.fotos[0].inmueble_id}" class='btn btn-info'><i class="fa-solid fa-eye"></i></a>
+                    </td>
+                </tr>`;
+        })
+        tableBody_reportes.innerHTML = content
+    } catch (ex) {
+        _alerta(ex)
+    }
 }
