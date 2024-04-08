61ca417448c34b68b5b7921bd9025c2845e625fd
dhubalde <dalilahubalde@hotmail.com>
2024-04-08 16:49:30 -0300

    conf form contacto 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = 'fdoisvthaztckzfe'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
