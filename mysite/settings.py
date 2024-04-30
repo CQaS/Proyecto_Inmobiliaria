c32811de98f58bfa748c59a692221a9328f96b7a
dhubalde <dalilahubalde@hotmail.com>
2024-04-30 20:37:42 -0300

    arreglo settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,8 +44,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER =config('EMAIL_HOST_USER')
 #'infoimoveismec@gmail.com'
 
-#ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['www.imoveismec.com.br']
+ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['www.imoveismec.com.br']
 
 # Application definition
 
@@ -102,10 +102,10 @@ DATABASES = {
             'sql_mode': 'traditional',
         },
         'NAME': 'inmobiliaria',
-        #'USER': 'root',
-        #'PASSWORD': '',
-        'USER': 'inmobiliariamec',
-        'PASSWORD': 'mec-2023',
+        'USER': 'root',
+        'PASSWORD': '',
+        #'USER': 'inmobiliariamec',
+        #'PASSWORD': 'mec-2023',
         'HOST': 'localhost'
     }
 }
